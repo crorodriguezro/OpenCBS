891742d5ba18e6b12b7c4b000a4ffe1d6d6000e7
Pavel Bastov <pbastov@gmail.com>
2013-06-11 16:10:26 +0600

    Add CustomPeriodPolicy.

---
+++ Src/OpenCBS.Engine/PeriodPolicy/CustomPeriodPolicy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using OpenCBS.Engine.Interfaces;
+
+namespace OpenCBS.Engine.PeriodPolicy
+{
+    public class CustomPeriodPolicy : IPeriodPolicy
+    {
+        private readonly int _numberOfDays;
+
+        public CustomPeriodPolicy(int numberOfDays)
+        {
+            _numberOfDays = numberOfDays;
+        }
+
+        public DateTime GetNextDate(DateTime date)
+        {
+            return date.AddDays(_numberOfDays);
+        }
+
+        public DateTime GetPreviousDate(DateTime date)
+        {
+            return date.AddDays(-_numberOfDays);
+        }
+
+        public int GetNumberOfDays(DateTime date)
+        {
+            return _numberOfDays;
+        }
+
+        public double GetNumberOfPeriodsInYear(DateTime date, IYearPolicy yearPolicy)
+        {
+            return (double) yearPolicy.GetNumberOfDays(date) / _numberOfDays;
+        }
+    }
+}
