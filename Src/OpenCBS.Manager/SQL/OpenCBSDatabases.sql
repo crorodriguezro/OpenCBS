ff1929c17111f0fd5e36dc76171ef2bc6f2be3bc
Pavel Bastov <pbastov@gmail.com>
2013-06-21 16:34:00 +0600

    Remove size (temporarily) size calculation from the database detection script.

--- Src/OpenCBS.Manager/SQL/OpenCBSDatabases.sql
+++ Src/OpenCBS.Manager/SQL/OpenCBSDatabases.sql
@@ -1,4 +1,3 @@
-DECLARE @userName NVARCHAR(MAX) = N'{0}'
 DECLARE @databases TABLE
 (
     Name NVARCHAR(MAX),
@@ -9,9 +8,6 @@ DECLARE @name NVARCHAR(MAX)
 DECLARE @version NVARCHAR(MAX)
 DECLARE @branch NVARCHAR(MAX)
 DECLARE @query NVARCHAR(MAX)
-DECLARE @total INT
-DECLARE @count INT
-DECLARE @hasDatabaseAccess BIT
 DECLARE databaseCursor CURSOR FOR
 SELECT name FROM sys.databases
 OPEN databaseCursor
@@ -21,23 +17,11 @@ WHILE @@FETCH_STATUS = 0
 BEGIN
     IF NOT OBJECT_ID(@name + '..TechnicalParameters') IS NULL
     BEGIN
-        SET @query = N'SELECT @versionOut = value FROM ' + @name + N'.dbo.TechnicalParameters WHERE name = ''version'''
+        SET @query = N'USE ' + @name + '; SELECT @versionOut = value FROM dbo.TechnicalParameters WHERE name = ''version'''
         EXEC sp_executesql @query, N'@versionOut NVARCHAR(MAX) OUTPUT', @versionOut = @version OUTPUT
-        SET @query = N'SELECT TOP 1 @branchOut = code FROM ' + @name + N'.dbo.Branches'
+        SET @query = N'USE ' + @name + ';SELECT TOP 1 @branchOut = code FROM dbo.Branches'
         EXEC sp_executesql @query, N'@branchOut NVARCHAR(MAX) OUTPUT', @branchOut = @branch OUTPUT
-        IF OBJECT_ID(N'dbo.OpenCBSDatabasePermissions', N'U') IS NOT NULL
-        BEGIN
-            SELECT @total = COUNT(*) FROM dbo.OpenCBSDatabasePermissions WHERE UserName = @userName
-            SELECT @count = COUNT(*) FROM dbo.OpenCBSDatabasePermissions WHERE UserName = @userName AND DatabaseName = @name
-            IF @total = 0 OR @count = 1
-            BEGIN
-                INSERT INTO @databases VALUES (@name, @version, @branch)
-            END
-        END
-        ELSE
-        BEGIN
-            INSERT INTO @databases VALUES (@name, @version, @branch)
-        END
+        INSERT INTO @databases VALUES (@name, @version, @branch)
     END
     FETCH databaseCursor INTO @name
 END
@@ -45,23 +29,23 @@ END
 CLOSE databaseCursor
 DEALLOCATE databaseCursor
 
-;
+--;
 
-WITH fs
-AS
-(
-    SELECT database_id, type, size * 8192 size
-    FROM sys.master_files
-)
+--WITH fs
+--AS
+--(
+--    SELECT database_id, type, size * 8192 size
+--    FROM sys.master_files
+--)
 
-SELECT db.Name, db.Version, db.BranchCode, dbs.DataFileSize, dbs.LogFileSize
+SELECT db.Name, db.Version, db.BranchCode, 0 DataFileSize, 0 LogFileSize
 FROM @databases db
-INNER JOIN 
-(
-    SELECT
-    name,
-    (SELECT SUM(size) FROM fs WHERE type = 0 AND fs.database_id = db.database_id) DataFileSize,
-    (SELECT SUM(size) FROM fs WHERE type = 1 AND fs.database_id = db.database_id) LogFileSize
-    FROM sys.databases db
-) dbs ON db.Name = dbs.name
+--INNER JOIN 
+--(
+--    SELECT
+--    name,
+--    (SELECT SUM(size) FROM fs WHERE type = 0 AND fs.database_id = db.database_id) DataFileSize,
+--    (SELECT SUM(size) FROM fs WHERE type = 1 AND fs.database_id = db.database_id) LogFileSize
+--    FROM sys.databases db
+--) dbs ON db.Name = dbs.name
 
