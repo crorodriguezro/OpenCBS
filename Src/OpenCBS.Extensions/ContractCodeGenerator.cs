e3b92639acc526ee8e41d602a30484bfdd1bd6ca
Pavel Bastov <pbastov@gmail.com>
2013-07-01 15:45:13 +0600

    Add a sample contract code generator.

--- Src/OpenCBS.Extensions/ContractCodeGenerator.cs
+++ Src/OpenCBS.Extensions/ContractCodeGenerator.cs
@@ -1,4 +1,6 @@
-﻿// This program is free software; you can redistribute it and/or modify
+﻿// Copyright © 2013 Open Octopus Ltd.
+//
+// This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation; either version 2 of the License, or
 // (at your option) any later version.
