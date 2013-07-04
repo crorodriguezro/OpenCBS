4fa41b9ab1745dd9c3b9f288471722f32c30e5f5
MakhsutIslamov <makhsut.islamov@gmail.com>
2013-07-04 12:48:46 +0600

    Add missing files for the reassign window.

---
+++ Src/OpenCBS.CoreDomain/Contracts/ReassignContractItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenCBS.CoreDomain.Contracts
+{
+    [Serializable]
+    public class ReassignContractItem
+    {
+        public char Type;
+
+        public int LoanId;
+
+        public string LoanCode;
+
+        public string LoanStatus;
+
+        public string ClientFirstName;
+
+        public string ClientLastName;
+
+        public decimal Amount;
+
+        public decimal OLB;
+
+        public decimal InterestRate;
+
+        public DateTime CreationDate;
+
+        public DateTime StartDate;
+
+        public DateTime CloseDate;
+
+        public string InstallmentTypes;
+
+        public string DistrictName;
+
+        public DateTime EffectDate;
+    }
+}
