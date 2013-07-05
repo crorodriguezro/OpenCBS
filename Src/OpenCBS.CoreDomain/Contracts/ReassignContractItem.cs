292b0ce73035dc94cf6cefa7ebc015822ca6b140
Pavel Bastov <pbastov@gmail.com>
2013-07-05 11:58:38 +0600

    Refactor ReassignContractItem: add header and convert fields to properties.

--- Src/OpenCBS.CoreDomain/Contracts/ReassignContractItem.cs
+++ Src/OpenCBS.CoreDomain/Contracts/ReassignContractItem.cs
@@ -1,38 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// Copyright © 2013 Open Octopus Ltd.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// Website: http://www.opencbs.com
+// Contact: contact@opencbs.com
+
+using System;
 
 namespace OpenCBS.CoreDomain.Contracts
 {
-    [Serializable]
     public class ReassignContractItem
     {
-        public char Type;
+        public char Type { get; set; }
 
-        public int LoanId;
+        public int LoanId { get; set; }
 
-        public string LoanCode;
+        public string LoanCode { get; set; }
 
-        public string LoanStatus;
+        public string LoanStatus { get; set; }
 
-        public string ClientFirstName;
+        public string ClientFirstName { get; set; }
 
-        public string ClientLastName;
+        public string ClientLastName { get; set; }
 
-        public decimal Amount;
+        public decimal Amount { get; set; }
 
-        public decimal OLB;
+        public decimal Olb { get; set; }
 
-        public decimal InterestRate;
+        public decimal InterestRate { get; set; }
 
-        public DateTime CreationDate;
+        public DateTime CreationDate { get; set; }
 
-        public DateTime StartDate;
+        public DateTime StartDate { get; set; }
 
-        public DateTime CloseDate;
+        public DateTime CloseDate { get; set; }
 
-        public string InstallmentTypes;
+        public string InstallmentTypes { get; set; }
 
         public string DistrictName;
 
