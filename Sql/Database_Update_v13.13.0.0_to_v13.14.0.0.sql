ffc7959f03af967fc68afb4e9c2b05a27259c195
Kubanychbek <kuban_aa@mail.ru>
2013-12-12 17:35:02 +0600

    Add non-accrual contract status

--- Sql/Database_Update_v13.13.0.0_to_v13.14.0.0.sql
+++ Sql/Database_Update_v13.13.0.0_to_v13.14.0.0.sql
@@ -1,3 +1,6 @@
+INSERT INTO [dbo].[Statuses] ([status_name]) VALUES ('Deleted')
+INSERT INTO [dbo].[Statuses] ([status_name]) VALUES ('Non-accrual')
+GO
 
 UPDATE  [TechnicalParameters]
 SET     [value] = 'v13.14.0.0'
