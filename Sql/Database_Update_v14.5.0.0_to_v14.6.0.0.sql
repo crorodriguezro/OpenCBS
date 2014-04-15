594491525d6b613a1ecbaab09896d99fd6877fbf
Nakai uulu Bek <bek15071991@mail.ru>
2014-04-15 09:50:18 +0600

    Delete options tab in roles section(#398)

--- Sql/Database_Update_v14.5.0.0_to_v14.6.0.0.sql
+++ Sql/Database_Update_v14.5.0.0_to_v14.6.0.0.sql
@@ -1,4 +1,17 @@
+IF  NOT EXISTS(SELECT *  FROM  INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Roles'  AND COLUMN_NAME = 'role_of_loan' AND COLUMN_NAME = 'role_of_saving' AND COLUMN_NAME = 'role_of_teller') 
+BEGIN
+ALTER TABLE [dbo].[Roles] DROP CONSTRAINT [DF__Roles__role_of_l__014935CB]
+ALTER TABLE [dbo].[Roles] DROP CONSTRAINT [DF__Roles__role_of_s__023D5A04]
+ALTER TABLE [dbo].[Roles] DROP CONSTRAINT [DF__Roles__role_of_t__03317E3D]
+
+ALTER TABLE  Roles DROP COLUMN  role_of_loan
+ALTER TABLE  Roles DROP COLUMN  role_of_saving
+ALTER TABLE  Roles DROP COLUMN  role_of_teller
+END
+GO
+ 
 UPDATE  [TechnicalParameters]
 SET     [value] = 'v14.6.0.0'
 WHERE   [name] = 'VERSION'
 GO
+  
