a90dbed759773ecea382f1522010fc0550f3e765
Nakai uulu Bek <bek15071991@mail.ru>
2014-04-15 11:02:45 +0600

    Fix update script

--- Sql/Database_Update_v14.5.0.0_to_v14.6.0.0.sql
+++ Sql/Database_Update_v14.5.0.0_to_v14.6.0.0.sql
@@ -1,9 +1,24 @@
 IF  NOT EXISTS(SELECT *  FROM  INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Roles'  AND COLUMN_NAME = 'role_of_loan' AND COLUMN_NAME = 'role_of_saving' AND COLUMN_NAME = 'role_of_teller') 
 BEGIN
-ALTER TABLE [dbo].[Roles] DROP CONSTRAINT [DF__Roles__role_of_l__014935CB]
-ALTER TABLE [dbo].[Roles] DROP CONSTRAINT [DF__Roles__role_of_s__023D5A04]
-ALTER TABLE [dbo].[Roles] DROP CONSTRAINT [DF__Roles__role_of_t__03317E3D]
+DECLARE @Str NVARCHAR(100)
+SET @Str = ( SELECT   name
+               FROM     sysobjects
+               WHERE    name LIKE 'DF__Roles__role_of_l%'
+             )
+EXEC('ALTER TABLE Roles DROP CONSTRAINT '+ @Str)
 
+SET @Str = ( SELECT   name
+               FROM     sysobjects
+               WHERE    name LIKE 'DF__Roles__role_of_s%'
+             )
+EXEC('ALTER TABLE Roles DROP CONSTRAINT '+ @Str)
+
+SET @Str = ( SELECT   name
+               FROM     sysobjects
+               WHERE    name LIKE 'DF__Roles__role_of_t%'
+             )
+EXEC('ALTER TABLE Roles DROP CONSTRAINT '+ @Str)
+ 
 ALTER TABLE  Roles DROP COLUMN  role_of_loan
 ALTER TABLE  Roles DROP COLUMN  role_of_saving
 ALTER TABLE  Roles DROP COLUMN  role_of_teller
