2a31f5402f6116d648865b1d5f23103d312a99c7
Kubanychbek <kuban_aa@mail.ru>
2014-07-03 10:26:17 +0600

    Fix interst rate rounding issue #479

--- Sql/Database_Update_v14.8.0.0_to_v14.9.0.0.sql
+++ Sql/Database_Update_v14.8.0.0_to_v14.9.0.0.sql
@@ -1,3 +1,22 @@
+UPDATE Credit 
+SET interest_rate = round(interest_rate,2)
+GO
+
+UPDATE Packages
+SET interest_rate = round(interest_rate,2),
+	interest_rate_min = round(interest_rate_min,2),
+	interest_rate_max = round(interest_rate_max,2)
+GO
+
+UPDATE ReschedulingOfALoanEvents
+SET previous_interest_rate = round(previous_interest_rate,2),
+	interest = round(interest,2)
+GO
+
+UPDATE TrancheEvents
+SET interest_rate = round(interest_rate,2)
+GO
+
 alter table RepaymentEvents
 add bounce_fee money not null default(0)
 GO
