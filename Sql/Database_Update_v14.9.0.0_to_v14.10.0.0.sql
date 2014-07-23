9034b6c1f91195d40907b80f9b007e27c6712fd2
Kubanychbek <kuban_aa@mail.ru>
2014-07-23 13:18:26 +0600

    Add booking table in demoDb and update script

--- Sql/Database_Update_v14.9.0.0_to_v14.10.0.0.sql
+++ Sql/Database_Update_v14.9.0.0_to_v14.10.0.0.sql
@@ -14,6 +14,42 @@ GO
 DElETE FROM GeneralParameters WHERE [key] ='USE_EXTERNAL_ACCOUNTING'
 GO
 
+CREATE TABLE [dbo].[Booking](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[DebitAccountId] [int] NULL,
+	[CreditAccountId] [int] NULL,
+	[Amount] [decimal](25, 15) NULL,
+	[Date] [datetime] NULL,
+	[LoanEventId] [int] NULL,
+	[SavingEventId] [int] NULL,
+	[LoanId] [int] NULL,
+	[ClientId] [int] NULL,
+	[UserId] [int] NULL,
+	[BranchId] [int] NULL,
+	[Description] [nvarchar](200) NULL,
+	[IsExported] [bit] NULL,
+	[IsDeleted] [bit] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[Booking] ADD  DEFAULT ((0)) FOR [LoanEventId]
+GO
+
+ALTER TABLE [dbo].[Booking] ADD  DEFAULT ((0)) FOR [SavingEventId]
+GO
+
+ALTER TABLE [dbo].[Booking] ADD  DEFAULT ((0)) FOR [IsExported]
+GO
+
+ALTER TABLE [dbo].[Booking] ADD  DEFAULT ((0)) FOR [IsDeleted]
+GO
+
+
 UPDATE  [TechnicalParameters]
 SET     [value] = 'v14.10.0.0'
 WHERE   [name] = 'VERSION'
