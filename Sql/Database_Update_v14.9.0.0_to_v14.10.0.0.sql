efa3fdfc64f645d5957aa00ffedf2d0552c45488
Nakai uulu Bek <bek15071991@mail.ru>
2014-07-28 17:00:19 +0600

    Add sql scripts for to create categories table, accounts table

--- Sql/Database_Update_v14.9.0.0_to_v14.10.0.0.sql
+++ Sql/Database_Update_v14.9.0.0_to_v14.10.0.0.sql
@@ -14,41 +14,58 @@ GO
 DElETE FROM GeneralParameters WHERE [key] ='USE_EXTERNAL_ACCOUNTING'
 GO
 
+
+IF not exists (SELECT * FROM sys.tables WHERE name = 'Booking')
+BEGIN
 CREATE TABLE [dbo].[Booking](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[DebitAccountId] [int] NULL,
 	[CreditAccountId] [int] NULL,
 	[Amount] [decimal](25, 15) NULL,
 	[Date] [datetime] NULL,
-	[LoanEventId] [int] NULL,
-	[SavingEventId] [int] NULL,
+	[LoanEventId] [int] NOT NULL DEFAULT(0),
+	[SavingEventId] [int] NOT NULL DEFAULT(0),
 	[LoanId] [int] NULL,
 	[ClientId] [int] NULL,
 	[UserId] [int] NULL,
 	[BranchId] [int] NULL,
 	[Description] [nvarchar](200) NULL,
-	[IsExported] [bit] NULL,
-	[IsDeleted] [bit] NULL,
+	[IsExported] [bit] NOT NULL DEFAULT(0),
+	[IsDeleted] [bit] NOT NULL DEFAULT(0)
 PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+END
 
 GO
 
-ALTER TABLE [dbo].[Booking] ADD  DEFAULT ((0)) FOR [LoanEventId]
+IF not exists (SELECT * FROM sys.tables WHERE name = 'Accounts')
+                                                BEGIN 
+                                                CREATE TABLE Accounts
+                                                (id int identity(1,1),
+                                                 account_number varchar(64),
+                                                 label nvarchar(256) not null,
+                                                 is_debit bit,
+                                                 id_category int,
+                                                 parent_id int
+                                                ) 
+END  
 GO
 
-ALTER TABLE [dbo].[Booking] ADD  DEFAULT ((0)) FOR [SavingEventId]
+IF not exists (SELECT * FROM sys.tables WHERE name = 'Categories')
+                                                BEGIN 
+                                                CREATE TABLE Categories
+												(
+												  Id int  identity(1,1),
+												  ParentId int,
+												  Name nvarchar(128),
+												  lft int ,
+												  rgt int
+												)
+END  
 GO
-
-ALTER TABLE [dbo].[Booking] ADD  DEFAULT ((0)) FOR [IsExported]
-GO
-
-ALTER TABLE [dbo].[Booking] ADD  DEFAULT ((0)) FOR [IsDeleted]
-GO
-
 
 UPDATE  [TechnicalParameters]
 SET     [value] = 'v14.10.0.0'
