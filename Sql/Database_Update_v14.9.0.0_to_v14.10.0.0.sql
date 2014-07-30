8e9bb317ff1ef7bfa8fed31aed14a3238256758d
Nakai uulu Bek <bek15071991@mail.ru>
2014-07-30 12:15:57 +0600

    Add sql script for filling accounts table and categories table

--- Sql/Database_Update_v14.9.0.0_to_v14.10.0.0.sql
+++ Sql/Database_Update_v14.9.0.0_to_v14.10.0.0.sql
@@ -73,6 +73,100 @@ DELETE FROM CustomFieldsValues where owner_id=0
 END
 GO
 
+
+IF((Select COUNT(*) from dbo.Categories) = 0 
+    or (Select COUNT(*) from dbo.Accounts) = 0)
+begin
+Insert into dbo.Categories VALUES (0, 'Assets', 0,0)
+Insert into dbo.Categories VALUES (0, 'Liabilities', 0,0)
+Insert into dbo.Categories VALUES (0, 'Income', 0,0)
+Insert into dbo.Categories VALUES (0, 'Expense', 0,0)
+Insert into dbo.Categories VALUES (0, 'Equity', 0,0)
+
+Insert into dbo.Categories VALUES (0, 'Expense', 0,0)
+Insert into dbo.Categories VALUES (0, 'Equity', 0,0)
+
+--Assets
+Insert into dbo.Accounts VALUES (100, 'Current Assets', 1,1,0)
+Insert into dbo.Accounts VALUES (101, 'Fixed Assets', 1,1,0)
+
+--100
+Insert into dbo.Accounts VALUES (1001, 'Bank', 1,0,1)
+Insert into dbo.Accounts VALUES (1002, 'Petty Cash', 1,0,1)
+Insert into dbo.Accounts VALUES (1003, 'Checking Account', 1,0,1)
+Insert into dbo.Accounts VALUES (1004, 'Short Term Investments', 1,0,1)
+Insert into dbo.Accounts VALUES (1005, 'Loan Receivables', 1,0,1)
+Insert into dbo.Accounts VALUES (1006, 'Accrued Interest Receivable', 1,0,1)
+Insert into dbo.Accounts VALUES (1007, 'Accumulated Loan Loss Provision', 0,0,1)
+Insert into dbo.Accounts VALUES (1008, 'Accounts Receivable', 1,0,1)
+Insert into dbo.Accounts VALUES (1009, 'Other Assets', 1,0,1)
+
+--1001
+Insert into dbo.Accounts VALUES (10010, 'Cash', 1,0,3)
+Insert into dbo.Accounts VALUES (10012, 'Cheque', 1,0,3)
+Insert into dbo.Accounts VALUES (10013, 'WT', 1,0,3)
+Insert into dbo.Accounts VALUES (10014, 'Transit', 1,0,3)
+
+--1005
+Insert into dbo.Accounts VALUES (10051, 'Current Loan Receivables', 1,0,7)
+Insert into dbo.Accounts VALUES (10052, 'Overdue Loan Receivables', 1,0,7)
+
+--1006
+Insert into dbo.Accounts VALUES (10061, 'Accrued Current Interest Receivable', 1,0,8)
+Insert into dbo.Accounts VALUES (10062, 'Accrued Late Interest Receivable', 1,0,8)
+
+--Liabilities
+Insert into dbo.Accounts VALUES (200, 'Current Liabilities', 0,2,0)
+
+--200
+Insert into dbo.Accounts VALUES (2001, 'Accounts Payable', 0,0,20)
+Insert into dbo.Accounts VALUES (2002, 'Accrued Expenses', 0,0,20)
+Insert into dbo.Accounts VALUES (2003, 'Payroll Liabities', 0,0,20)
+Insert into dbo.Accounts VALUES (2004, 'Accrued Interest Expense', 0,0,20)
+Insert into dbo.Accounts VALUES (2005, 'Accrued Payroll Expense', 0,0,20)
+Insert into dbo.Accounts VALUES (2006, 'Accrued Payroll Taxes', 0,0,20)
+Insert into dbo.Accounts VALUES (2007, 'Federal Income Tax Payable', 0,0,20)
+Insert into dbo.Accounts VALUES (2008, 'State/Local Income Tax Payable', 0,0,20)
+Insert into dbo.Accounts VALUES (2009, 'Line of Credit', 0,0,20)
+Insert into dbo.Accounts VALUES (2010, 'Term Loan', 0,0,20)
+Insert into dbo.Accounts VALUES (2011, 'Note Payable', 0,0,20)
+Insert into dbo.Accounts VALUES (2012, 'Debentures', 0,0,20)
+
+--Income 
+Insert into dbo.Accounts VALUES (300, 'Income', 0,3,0)
+
+--300
+Insert into dbo.Accounts VALUES (3001, 'Interest Income', 0,0,33)
+Insert into dbo.Accounts VALUES (3002, 'Processing Fees', 0,0,33)
+Insert into dbo.Accounts VALUES (3003, 'Late Fees', 0,0,33)
+Insert into dbo.Accounts VALUES (3004, 'Misc. Fees', 0,0,33)
+Insert into dbo.Accounts VALUES (3005, 'Recovery Income', 0,0,33)
+Insert into dbo.Accounts VALUES (3006, 'Finance Income', 0,0,33)
+Insert into dbo.Accounts VALUES (3007, 'Other Income', 0,0,33)
+Insert into dbo.Accounts VALUES (3008, 'Accrued Interest', 0,0,33)
+
+--3001
+Insert into dbo.Accounts VALUES (30011, 'Current Interest', 0,0,34)
+Insert into dbo.Accounts VALUES (30012, 'Late Interest', 0,0,34)
+
+--Expense
+Insert into dbo.Accounts VALUES (400, 'Expense', 1,4,0)
+
+--400
+Insert into dbo.Accounts VALUES (4001, 'Interest Expense', 1,0,44)
+Insert into dbo.Accounts VALUES (4002, 'Loan Loss Provisioning', 1,0,44)
+Insert into dbo.Accounts VALUES (4003, 'Penalties', 1,0,44)
+Insert into dbo.Accounts VALUES (4004, 'Professional Fees', 1,0,44)
+
+--Equity
+Insert into dbo.Accounts VALUES (500, 'Equity', 0,5,0)
+
+--500
+Insert into dbo.Accounts VALUES (5001, 'Common Shares', 0,0,49)
+Insert into dbo.Accounts VALUES (5002, 'Paid in Capital',0,0,49)
+end
+GO
+
 UPDATE  [TechnicalParameters]
 SET     [value] = 'v14.10.0.0'
 WHERE   [name] = 'VERSION'
