7adbe1f287bcc029d33c328fa270fe41afef33a0
AdiletToigonbaev <adilet_kg93@mail.ru>
2015-01-09 14:42:31 +0600

    Removing errors in the update script

--- Sql/Database_Update_v14.15.0.0_to_v14.16.0.0.sql
+++ Sql/Database_Update_v14.15.0.0_to_v14.16.0.0.sql
@@ -1,13 +1,9 @@
 alter table dbo.Persons
 add uuid uniqueidentifier not null default(newid())
 
-<<<<<<< HEAD
-=======
 alter table dbo.Branches
 add is_default bit not null default(0)
 
-
->>>>>>> Adding the possibility to set default date format
 INSERT INTO dbo.GeneralParameters ([key], value)
 values ('SHORT_DATE_FORMAT', 'dd-MM-yyyy')
 
