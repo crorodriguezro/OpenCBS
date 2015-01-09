036357766ae16e8daa9cd1648778c97789d3c17d
AdiletToigonbaev <adilet_kg93@mail.ru>
2015-01-09 14:49:24 +0600

    Removing errors in the update script 15.1

--- Sql/Database_Update_v14.16.0.0_to_v15.1.0.0.sql
+++ Sql/Database_Update_v14.16.0.0_to_v15.1.0.0.sql
@@ -1,3 +1,6 @@
+INSERT INTO dbo.GeneralParameters ([key], value)
+values ('SHORT_DATE_FORMAT', 'dd-MM-yyyy')
+
 UPDATE  [TechnicalParameters]
 SET     [value] = 'v15.1.0.0'
 WHERE   [name] = 'VERSION'
