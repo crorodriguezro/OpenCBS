5046d11f55981b91d8337efd2dfa02a52bd630db
EmilyWatson <empcwm@gmail.com>
2015-01-26 16:22:01 +0600

    Adding translations of LoanApprovalUserControl

--- Src/OpenCBS.Extensions/LoanApprovalUserControl.Designer.cs
+++ Src/OpenCBS.Extensions/LoanApprovalUserControl.Designer.cs
@@ -31,6 +31,7 @@ namespace OpenCBS.Extensions
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoanApprovalUserControl));
             this.tableLayoutPanel11 = new System.Windows.Forms.TableLayoutPanel();
             this.lblCCStatus = new System.Windows.Forms.Label();
             this.pnlCCStatus = new System.Windows.Forms.Panel();
@@ -51,11 +52,7 @@ namespace OpenCBS.Extensions
             // 
             // tableLayoutPanel11
             // 
-            this.tableLayoutPanel11.AutoSize = true;
-            this.tableLayoutPanel11.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.tableLayoutPanel11.ColumnCount = 2;
-            this.tableLayoutPanel11.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel11.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            resources.ApplyResources(this.tableLayoutPanel11, "tableLayoutPanel11");
             this.tableLayoutPanel11.Controls.Add(this.lblCCStatus, 0, 0);
             this.tableLayoutPanel11.Controls.Add(this.pnlCCStatus, 1, 0);
             this.tableLayoutPanel11.Controls.Add(this.labelCreditCommiteeDate, 0, 1);
@@ -64,159 +61,90 @@ namespace OpenCBS.Extensions
             this.tableLayoutPanel11.Controls.Add(this.labelCreditCommiteeCode, 0, 2);
             this.tableLayoutPanel11.Controls.Add(this._commentTextBox, 1, 3);
             this.tableLayoutPanel11.Controls.Add(this._codeTextBox, 1, 2);
-            this.tableLayoutPanel11.Dock = System.Windows.Forms.DockStyle.Top;
-            this.tableLayoutPanel11.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel11.Name = "tableLayoutPanel11";
-            this.tableLayoutPanel11.RowCount = 4;
-            this.tableLayoutPanel11.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel11.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel11.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel11.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel11.Size = new System.Drawing.Size(757, 333);
-            this.tableLayoutPanel11.TabIndex = 69;
             // 
             // lblCCStatus
             // 
-            this.lblCCStatus.AutoSize = true;
+            resources.ApplyResources(this.lblCCStatus, "lblCCStatus");
             this.lblCCStatus.BackColor = System.Drawing.Color.Transparent;
-            this.lblCCStatus.Dock = System.Windows.Forms.DockStyle.Left;
-            this.lblCCStatus.Font = new System.Drawing.Font("Arial", 9.75F);
-            this.lblCCStatus.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.lblCCStatus.Location = new System.Drawing.Point(3, 0);
             this.lblCCStatus.Name = "lblCCStatus";
-            this.lblCCStatus.Size = new System.Drawing.Size(46, 33);
-            this.lblCCStatus.TabIndex = 37;
-            this.lblCCStatus.Text = "Status";
-            this.lblCCStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // pnlCCStatus
             // 
-            this.pnlCCStatus.AutoSize = true;
+            resources.ApplyResources(this.pnlCCStatus, "pnlCCStatus");
             this.pnlCCStatus.Controls.Add(this._statusComboBox);
-            this.pnlCCStatus.Location = new System.Drawing.Point(73, 3);
             this.pnlCCStatus.Name = "pnlCCStatus";
-            this.pnlCCStatus.Size = new System.Drawing.Size(302, 27);
-            this.pnlCCStatus.TabIndex = 69;
             // 
             // _statusComboBox
             // 
+            resources.ApplyResources(this._statusComboBox, "_statusComboBox");
             this._statusComboBox.DisplayMember = "Value";
             this._statusComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this._statusComboBox.FormattingEnabled = true;
-            this._statusComboBox.Location = new System.Drawing.Point(0, 0);
             this._statusComboBox.Name = "_statusComboBox";
-            this._statusComboBox.Size = new System.Drawing.Size(299, 24);
-            this._statusComboBox.TabIndex = 4;
             this._statusComboBox.ValueMember = "Key";
             // 
             // labelCreditCommiteeDate
             // 
-            this.labelCreditCommiteeDate.AutoSize = true;
+            resources.ApplyResources(this.labelCreditCommiteeDate, "labelCreditCommiteeDate");
             this.labelCreditCommiteeDate.BackColor = System.Drawing.Color.Transparent;
-            this.labelCreditCommiteeDate.Font = new System.Drawing.Font("Arial", 9.75F);
-            this.labelCreditCommiteeDate.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.labelCreditCommiteeDate.Location = new System.Drawing.Point(3, 33);
             this.labelCreditCommiteeDate.Name = "labelCreditCommiteeDate";
-            this.labelCreditCommiteeDate.Size = new System.Drawing.Size(35, 16);
-            this.labelCreditCommiteeDate.TabIndex = 18;
-            this.labelCreditCommiteeDate.Text = "Date";
-            this.labelCreditCommiteeDate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // _dateTimePicker
             // 
+            resources.ApplyResources(this._dateTimePicker, "_dateTimePicker");
             this._dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this._dateTimePicker.CustomFormat = ApplicationSettings.GetInstance("").SHORT_DATE_FORMAT;
-            this._dateTimePicker.Location = new System.Drawing.Point(73, 36);
             this._dateTimePicker.Name = "_dateTimePicker";
-            this._dateTimePicker.Size = new System.Drawing.Size(125, 22);
-            this._dateTimePicker.TabIndex = 0;
             // 
             // labelCreditCommiteeComment
             // 
-            this.labelCreditCommiteeComment.AutoSize = true;
+            resources.ApplyResources(this.labelCreditCommiteeComment, "labelCreditCommiteeComment");
             this.labelCreditCommiteeComment.BackColor = System.Drawing.Color.Transparent;
-            this.labelCreditCommiteeComment.Font = new System.Drawing.Font("Arial", 9.75F);
-            this.labelCreditCommiteeComment.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.labelCreditCommiteeComment.Location = new System.Drawing.Point(3, 89);
             this.labelCreditCommiteeComment.Name = "labelCreditCommiteeComment";
-            this.labelCreditCommiteeComment.Size = new System.Drawing.Size(64, 16);
-            this.labelCreditCommiteeComment.TabIndex = 20;
-            this.labelCreditCommiteeComment.Text = "Comment";
-            this.labelCreditCommiteeComment.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // labelCreditCommiteeCode
             // 
-            this.labelCreditCommiteeCode.AutoSize = true;
+            resources.ApplyResources(this.labelCreditCommiteeCode, "labelCreditCommiteeCode");
             this.labelCreditCommiteeCode.BackColor = System.Drawing.Color.Transparent;
-            this.labelCreditCommiteeCode.Font = new System.Drawing.Font("Arial", 9.75F);
-            this.labelCreditCommiteeCode.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.labelCreditCommiteeCode.Location = new System.Drawing.Point(3, 61);
             this.labelCreditCommiteeCode.Name = "labelCreditCommiteeCode";
-            this.labelCreditCommiteeCode.Size = new System.Drawing.Size(38, 16);
-            this.labelCreditCommiteeCode.TabIndex = 35;
-            this.labelCreditCommiteeCode.Text = "Code";
-            this.labelCreditCommiteeCode.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // _commentTextBox
             // 
-            this._commentTextBox.Dock = System.Windows.Forms.DockStyle.Top;
-            this._commentTextBox.Location = new System.Drawing.Point(73, 92);
-            this._commentTextBox.MaxLength = 4000;
-            this._commentTextBox.Multiline = true;
+            resources.ApplyResources(this._commentTextBox, "_commentTextBox");
             this._commentTextBox.Name = "_commentTextBox";
-            this._commentTextBox.Size = new System.Drawing.Size(681, 238);
-            this._commentTextBox.TabIndex = 2;
             // 
             // _codeTextBox
             // 
-            this._codeTextBox.Location = new System.Drawing.Point(73, 64);
+            resources.ApplyResources(this._codeTextBox, "_codeTextBox");
             this._codeTextBox.Name = "_codeTextBox";
-            this._codeTextBox.Size = new System.Drawing.Size(237, 22);
-            this._codeTextBox.TabIndex = 1;
             // 
             // flowLayoutPanel7
             // 
-            this.flowLayoutPanel7.AutoSize = true;
-            this.flowLayoutPanel7.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            resources.ApplyResources(this.flowLayoutPanel7, "flowLayoutPanel7");
             this.flowLayoutPanel7.Controls.Add(this._saveButton);
             this.flowLayoutPanel7.Controls.Add(this._printButton);
-            this.flowLayoutPanel7.Dock = System.Windows.Forms.DockStyle.Top;
-            this.flowLayoutPanel7.Location = new System.Drawing.Point(0, 333);
             this.flowLayoutPanel7.Name = "flowLayoutPanel7";
-            this.flowLayoutPanel7.Padding = new System.Windows.Forms.Padding(0, 13, 0, 0);
-            this.flowLayoutPanel7.Size = new System.Drawing.Size(757, 47);
-            this.flowLayoutPanel7.TabIndex = 70;
             // 
             // _saveButton
             // 
-            this._saveButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this._saveButton.Location = new System.Drawing.Point(3, 16);
+            resources.ApplyResources(this._saveButton, "_saveButton");
             this._saveButton.Name = "_saveButton";
-            this._saveButton.Size = new System.Drawing.Size(140, 28);
-            this._saveButton.TabIndex = 1;
-            this._saveButton.Text = "Save";
             // 
             // _printButton
             // 
+            resources.ApplyResources(this._printButton, "_printButton");
             this._printButton.Image = global::OpenCBS.Extensions.Properties.Resources.bullet_arrow_down;
-            this._printButton.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this._printButton.Location = new System.Drawing.Point(149, 16);
             this._printButton.Name = "_printButton";
             this._printButton.ReportInitializer = null;
-            this._printButton.Size = new System.Drawing.Size(140, 28);
-            this._printButton.TabIndex = 2;
-            this._printButton.Text = "Print";
             this._printButton.UseVisualStyleBackColor = true;
             // 
             // LoanApprovalUserControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.flowLayoutPanel7);
             this.Controls.Add(this.tableLayoutPanel11);
-            this.Font = new System.Drawing.Font("Arial", 9.75F);
             this.Name = "LoanApprovalUserControl";
-            this.Size = new System.Drawing.Size(757, 398);
             this.tableLayoutPanel11.ResumeLayout(false);
             this.tableLayoutPanel11.PerformLayout();
             this.pnlCCStatus.ResumeLayout(false);
