4147857bfe4822a681fd579767a1dd4642b2c814
EmilyWatson <empcwm@gmail.com>
2015-01-26 09:41:52 +0600

    Removing possible duplicate primary key issue
    Short Date Format pk is added in the next sql update i.e. 14.16 to 15.1

--- Sql/Database_Update_v14.15.0.0_to_v14.16.0.0.sql
+++ Sql/Database_Update_v14.15.0.0_to_v14.16.0.0.sql
@@ -4,9 +4,6 @@ add uuid uniqueidentifier not null default(newid())
 alter table dbo.Branches
 add is_default bit not null default(0)
 
-INSERT INTO dbo.GeneralParameters ([key], value)
-values ('SHORT_DATE_FORMAT', 'dd-MM-yyyy')
-
 UPDATE  [TechnicalParameters]
 SET     [value] = 'v14.16.0.0'
 WHERE   [name] = 'VERSION'
