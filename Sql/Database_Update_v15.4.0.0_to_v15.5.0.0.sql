072e05d1b0b487554c2bcd146a98197da3578660
EmilyWatson <empcwm@gmail.com>
2015-04-23 14:11:16 +0600

    Adding View menu item into Roles security staff #776

--- Sql/Database_Update_v15.4.0.0_to_v15.5.0.0.sql
+++ Sql/Database_Update_v15.4.0.0_to_v15.5.0.0.sql
@@ -1,3 +1,18 @@
+if exists(select * from sys.columns 
+            where Name = N'type_id' and Object_ID = Object_ID(N'city'))
+begin
+    alter table city 
+	add default '1' for type_id
+end
+GO
+
+insert into MenuItems (component_name, type)
+values ('_viewItem', 0)
+, ('_startPageItem', 0)
+, ('_alertsItem' , 0)
+, ('_dashboardItem', 0)
+GO
+
 UPDATE  [TechnicalParameters]
 SET     [value] = 'v15.5.0.0'
 WHERE   [name] = 'VERSION'
