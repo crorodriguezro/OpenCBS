50abb76ba3443f0a6777dc3c8f757d6b39165f09
EmilyWatson <empcwm@gmail.com>
2015-05-12 12:03:01 +0600

    Changing the update script.

--- Sql/Database_Update_v15.4.0.0_to_v15.5.0.0.sql
+++ Sql/Database_Update_v15.4.0.0_to_v15.5.0.0.sql
@@ -6,23 +6,23 @@ begin
 end
 GO
 
-insert into MenuItems (component_name, type)
-values ('_viewItem', 0)
+INSERT INTO MenuItems ([component_name], [type])
+VALUES ('_viewItem', 0)
 , ('_startPageItem', 0)
 , ('_alertsItem' , 0)
 , ('_dashboardItem', 0)
 GO
 
-insert into menuitems(component_name, [type])
- values ('_modulesMenuItem', 0)
- , ('_aboutModulesMenuItem', 0)
- , ('mnuCustomFields', 0)
- , ('ClientLocation', 0)
- , ('mnuEventFields', 0)
- , ('DMM', 0)
- , ('FastRepaymentMenuItem', 0)
- , ('TaskManagementMenuItem', 0)
- GO
+INSERT INTO menuitems([component_name], [type])
+VALUES ('_modulesMenuItem', 0)
+, ('_aboutModulesMenuItem', 0)
+, ('mnuCustomFields', 0)
+, ('ClientLocation', 0)
+, ('mnuEventFields', 0)
+, ('DMM', 0)
+, ('FastRepaymentMenuItem', 0)
+, ('TaskManagementMenuItem', 0)
+GO
 
 UPDATE  [TechnicalParameters]
 SET     [value] = 'v15.5.0.0'
