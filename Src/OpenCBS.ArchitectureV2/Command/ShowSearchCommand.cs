25327fa00e579f4d400ff8409ed2dc5b04c10eba
Pavel Bastov <pbastov@gmail.com>
2015-05-20 11:07:35 +0600

    Add search command (wip)

---
+++ Src/OpenCBS.ArchitectureV2/Command/ShowSearchCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using OpenCBS.ArchitectureV2.Interface;
+
+namespace OpenCBS.ArchitectureV2.Command
+{
+    public class ShowSearchCommand : ICommand<ShowSearchCommand>
+    {
+        public void Execute(ShowSearchCommand commandData)
+        {
+        }
+    }
+}
