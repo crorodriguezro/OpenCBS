d52d7db3dc5d63ee42062bbac4da282773cda43d
Kubanychbek <kuban_aa@mail.ru>
2015-06-02 10:35:46 +0600

    Bump to 15.6

--- Src/SolutionInfo.Template.cs
+++ Src/SolutionInfo.Template.cs
@@ -22,7 +22,7 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Open Octopus Ltd.")]
 [assembly: AssemblyProduct("OPENCBS")]
-[assembly: AssemblyVersion("15.5.build.0")]
+[assembly: AssemblyVersion("15.6.build.0")]
 [assembly: AssemblyGitRevision("revision")]
 
 [AttributeUsage(AttributeTargets.Assembly)]
