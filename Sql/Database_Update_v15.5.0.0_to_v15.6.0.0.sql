160f6b9d613a90630d11a045c3838906f8e97ddd
EmilyWatson <empcwm@gmail.com>
2015-06-10 15:39:24 +0600

    Anticipating possible upgrade errors and avoiding them

--- Sql/Database_Update_v15.5.0.0_to_v15.6.0.0.sql
+++ Sql/Database_Update_v15.5.0.0_to_v15.6.0.0.sql
@@ -26,41 +26,140 @@ if object_id('DF_Persons_povertylevel_economiceducation') is not null
 alter table dbo.Persons drop constraint DF_Persons_povertylevel_economiceducation
 GO
 
-ALTER TABLE Persons DROP COLUMN 
-  household_head
-, nb_of_dependents
-, nb_of_children
-, children_basic_education
-, livestock_number
-, livestock_type
-, landplot_size
-, home_size
-, home_time_living_in
-, capital_other_equipments
-, experience
-, nb_of_people
-, monthly_income
-, monthly_expenditure
-, comments
-, study_level
-, SS
-, CAF
-, housing_situation
-, bank_situation
-, handicapped
-, professional_experience
-, first_contact
-, family_situation
-, mother_name
-, povertylevel_childreneducation
-, povertylevel_socialparticipation
-, povertylevel_healthsituation
-, unemployment_months
-, personalBank_id
-, businessBank_id
-, professional_situation
-, povertylevel_economiceducation
-, first_appointment
+if exists(select * from sys.columns where Name = N'household_head' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column household_head
+GO
+
+if exists(select * from sys.columns where Name = N'nb_of_dependents' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column nb_of_dependents
+GO
+
+if exists(select * from sys.columns where Name = N'nb_of_children' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column nb_of_children
+GO
+
+if exists(select * from sys.columns where Name = N'children_basic_education' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column children_basic_education
+GO
+
+if exists(select * from sys.columns where Name = N'livestock_number' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column livestock_number
+GO
+
+if exists(select * from sys.columns where Name = N'livestock_type' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column livestock_type
+GO
+
+if exists(select * from sys.columns where Name = N'landplot_size' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column landplot_size
+GO
+
+if exists(select * from sys.columns where Name = N'home_size' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column home_size
+GO
+
+if exists(select * from sys.columns where Name = N'home_time_living_in' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column home_time_living_in
+GO
+
+if exists(select * from sys.columns where Name = N'capital_other_equipments' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column capital_other_equipments
+GO
+
+if exists(select * from sys.columns where Name = N'experience' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column experience
+GO
+
+if exists(select * from sys.columns where Name = N'nb_of_people' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column nb_of_people
+GO
+
+if exists(select * from sys.columns where Name = N'monthly_income' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column monthly_income
+GO
+
+if exists(select * from sys.columns where Name = N'monthly_expenditure' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column monthly_expenditure
+GO
+
+if exists(select * from sys.columns where Name = N'comments' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column comments
+GO
+
+if exists(select * from sys.columns where Name = N'study_level' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column study_level
+GO
+
+if exists(select * from sys.columns where Name = N'SS' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column SS
+GO
+
+if exists(select * from sys.columns where Name = N'CAF' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column CAF
+GO
+
+if exists(select * from sys.columns where Name = N'housing_situation' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column housing_situation
+GO
+
+if exists(select * from sys.columns where Name = N'bank_situation' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column bank_situation
+GO
+
+if exists(select * from sys.columns where Name = N'handicapped' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column handicapped
+GO
+
+if exists(select * from sys.columns where Name = N'professional_experience' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column professional_experience
+GO
+
+if exists(select * from sys.columns where Name = N'first_contact' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column first_contact
+GO
+
+if exists(select * from sys.columns where Name = N'family_situation' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column family_situation
+GO
+
+if exists(select * from sys.columns where Name = N'mother_name' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column mother_name
+GO
+
+if exists(select * from sys.columns where Name = N'povertylevel_childreneducation' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column povertylevel_childreneducation
+GO
+
+if exists(select * from sys.columns where Name = N'povertylevel_socialparticipation' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column povertylevel_socialparticipation
+GO
+
+if exists(select * from sys.columns where Name = N'povertylevel_healthsituation' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column povertylevel_healthsituation
+GO
+
+if exists(select * from sys.columns where Name = N'unemployment_months' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column unemployment_months
+GO
+
+if exists(select * from sys.columns where Name = N'personalBank_id' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column personalBank_id
+GO
+
+if exists(select * from sys.columns where Name = N'businessBank_id' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column businessBank_id
+GO
+
+if exists(select * from sys.columns where Name = N'professional_situation' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column professional_situation
+GO
+
+if exists(select * from sys.columns where Name = N'povertylevel_economiceducation' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column povertylevel_economiceducation
+GO
+
+if exists(select * from sys.columns where Name = N'first_appointment' and Object_ID = Object_ID(N'dbo.Persons'))
+alter table dbo.Persons drop column first_appointment
 GO
 
 delete from 
