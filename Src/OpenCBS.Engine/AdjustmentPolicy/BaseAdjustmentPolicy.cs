a6c50d5900895392bea86e69b73d998cf89b87e8
Askat Asanaliev <aaskat@bitbucket.org>
2015-08-05 10:52:43 +0600

    Fixed bug in schedule, improve adjustment last installment in flat schedule

--- Src/OpenCBS.Engine/AdjustmentPolicy/BaseAdjustmentPolicy.cs
+++ Src/OpenCBS.Engine/AdjustmentPolicy/BaseAdjustmentPolicy.cs
@@ -1,6 +1,8 @@
 ﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using OpenCBS.CoreDomain.Contracts.Loans.Installments;
 using OpenCBS.Engine.Interfaces;
 
@@ -11,6 +13,17 @@ namespace OpenCBS.Engine.AdjustmentPolicy
         public decimal GetAdjustment(List<Installment> schedule, IScheduleConfiguration configuration)
         {
             return configuration.Amount - schedule.Sum(i => i.CapitalRepayment.Value);
+        }
+
+        public decimal GetAdjustmentInterestRepayment(List<Installment> schedule, IScheduleConfiguration configuration)
+        {
+            var numberOfPeriods =
+                (decimal)
+                    (configuration.PeriodPolicy.GetNumberOfPeriodsInYear(
+                        configuration.PreferredFirstInstallmentDate, configuration.YearPolicy));
+            return Math.Round((configuration.Amount*configuration.InterestRate/numberOfPeriods/100)*
+                              configuration.NumberOfInstallments, 0) -
+                   schedule.Sum(i => i.InterestsRepayment.Value);
         }
     }
 }
--- Src/OpenCBS.Engine/AdjustmentPolicy/LastInstallmentAdjustmentPolicy.cs
+++ Src/OpenCBS.Engine/AdjustmentPolicy/LastInstallmentAdjustmentPolicy.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using OpenCBS.CoreDomain.Contracts.Loans.Installments;
+using OpenCBS.Engine.InstallmentCalculationPolicy;
 using OpenCBS.Engine.Interfaces;
 
 namespace OpenCBS.Engine.AdjustmentPolicy
 {
-    [Export(typeof(IPolicy))]
+    [Export(typeof (IPolicy))]
     [PolicyAttribute(Implementation = "Last installment")]
     public class LastInstallmentAdjustmentPolicy : BaseAdjustmentPolicy, IAdjustmentPolicy
     {
         public void Adjust(List<Installment> schedule, IScheduleConfiguration configuration)
         {
             schedule[schedule.Count - 1].CapitalRepayment += GetAdjustment(schedule, configuration);
+            if (configuration.CalculationPolicy.GetType() == typeof (FlatInstallmentCalculationPolicy))
+                schedule[schedule.Count - 1].InterestsRepayment += GetAdjustmentInterestRepayment(schedule,
+                    configuration);
         }
     }
 }
