120cdba94cb8353852d3be61b0e9f6023909dc2c
Adilet Toigonbaev <toigonbaevadilet@gmail.com>
2015-08-25 18:16:52 +0600

    Fixing bug when New client button allows to create clients ignoring role's possibiilties #21

--- Src/OpenCBS.ArchitectureV2/View/StartPageView.cs
+++ Src/OpenCBS.ArchitectureV2/View/StartPageView.cs
@@ -1,15 +1,21 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
 using OpenCBS.ArchitectureV2.Interface.Presenter;
 using OpenCBS.ArchitectureV2.Interface.Service;
 using OpenCBS.ArchitectureV2.Interface.View;
+using OpenCBS.CoreDomain;
+using OpenCBS.Enums;
+using OpenCBS.Services;
 using StructureMap;
 
 namespace OpenCBS.ArchitectureV2.View
 {
     public partial class StartPageView : BaseView, IStartPageView
     {
+        private List<MenuObject> _menuItems;
         [DefaultConstructor]
+
         public StartPageView(ITranslationService translationService) : base(translationService)
         {
             InitializeComponent();
@@ -20,9 +26,32 @@ namespace OpenCBS.ArchitectureV2.View
             new ToolTip().SetToolTip(_spanishPictureBox, "Español");
             new ToolTip().SetToolTip(_portuguesePictureBox, "Português");
             _newClientButton.Click += (sender, e) =>
-            {
+            {   
                 _newClientMenu.Show(_newClientButton, 0, _newClientButton.Height);
             };
+            _InitializeUserRights();
+        }
+
+        private void _InitializeUserRights()
+        {
+            _menuItems = ServicesProvider.GetInstance().GetMenuItemServices().GetMenuList(OSecurityObjectTypes.MenuItem);
+            var role = User.CurrentUser.UserRole;
+            var itemName = "mnuNewPerson";
+            var foundMo = _menuItems.Find(item => item == itemName.Trim());
+            if (foundMo != null)
+                _newPersonItem.Enabled = role.IsMenuAllowed(foundMo);
+            itemName = "mnuNewGroup";
+            foundMo = _menuItems.Find(item => item == itemName.Trim());
+            if (foundMo != null)
+                _newGroupItem.Enabled = role.IsMenuAllowed(foundMo);
+            itemName = "mnuNewVillage";
+            foundMo = _menuItems.Find(item => item == itemName.Trim());
+            if (foundMo != null)
+                _newVillageBankItem.Enabled = role.IsMenuAllowed(foundMo);
+            itemName = "newCorporateToolStripMenuItem";
+            foundMo = _menuItems.Find(item => item == itemName.Trim());
+            if (foundMo != null)
+                _newCompanyItem.Enabled = role.IsMenuAllowed(foundMo);
         }
 
         public void Attach(IStartPagePresenterCallbacks presenterCallbacks)
