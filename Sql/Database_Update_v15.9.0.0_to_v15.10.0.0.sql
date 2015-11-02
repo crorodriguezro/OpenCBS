fe43b47ec6ea261c052dfc02e8fad1d4813edb42
Askat Asanaliev <aaskat@bitbucket.org>
2015-11-02 14:59:33 +0600

    Change go to GO

--- Sql/Database_Update_v15.9.0.0_to_v15.10.0.0.sql
+++ Sql/Database_Update_v15.9.0.0_to_v15.10.0.0.sql
@@ -1,6 +1,6 @@
 ALTER TABLE dbo.ContractEvents ADD doc1 VARCHAR(255) NULL;
 ALTER TABLE dbo.ContractEvents ADD doc2 VARCHAR(255) NULL;
-go
+Go
 
 UPDATE  [TechnicalParameters]
 SET     [value] = 'v15.10.0.0'
