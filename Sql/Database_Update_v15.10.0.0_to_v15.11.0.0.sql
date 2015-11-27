53bd0d461ad3a545d5aef4604398105fe3a46592
Pavel Bastov <pbastov@gmail.com>
2015-11-27 11:29:10 +0600

    Add last interest accrual date
    Resolves: #7

--- Sql/Database_Update_v15.10.0.0_to_v15.11.0.0.sql
+++ Sql/Database_Update_v15.10.0.0_to_v15.11.0.0.sql
@@ -7,6 +7,18 @@ GO
 ALTER TABLE dbo.InstallmentHistory ADD extraAmount2 MONEY NULL DEFAULT(0)
 GO
 
+alter table dbo.Installments add last_interest_accrual_date date NULL
+GO
+
+alter table dbo.InstallmentHistory add last_interest_accrual_date date NULL
+GO
+
+update dbo.Installments set last_interest_accrual_date = expected_date
+GO
+
+update dbo.InstallmentHistory set last_interest_accrual_date = expected_date
+GO
+
 UPDATE  [TechnicalParameters]
 SET     [value] = 'v15.11.0.0'
 WHERE   [name] = 'VERSION'
