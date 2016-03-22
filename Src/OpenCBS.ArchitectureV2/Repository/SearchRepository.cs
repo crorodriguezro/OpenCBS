90ff1a6e4c5678bc9f43c9dcfcec263896e56ce0
Alexei <mrbrealmadrid@gamil.com>
2016-03-22 17:07:18 +0600

    Village Bank Search Filter

--- Src/OpenCBS.ArchitectureV2/Repository/SearchRepository.cs
+++ Src/OpenCBS.ArchitectureV2/Repository/SearchRepository.cs
@@ -4,6 +4,8 @@ using OpenCBS.ArchitectureV2.Interface;
 using OpenCBS.ArchitectureV2.Interface.Repository;
 using OpenCBS.ArchitectureV2.Model;
 using Dapper;
+using OpenCBS.CoreDomain;
+using OpenCBS.Services;
 
 namespace OpenCBS.ArchitectureV2.Repository
 {
@@ -32,15 +34,21 @@ namespace OpenCBS.ArchitectureV2.Repository
 	                dbo.Tiers t on v.id = t.id
                 left join
 	                dbo.Districts d on d.id = t.district_id
+                left join
+	                dbo.UsersBranches ub on ub.user_id = @userId
+                left join
+	                dbo.Branches b on b.id = ub.branch_id
                 where
 	                v.name like @keywords
+                and 
+                    t.branch_id = b.id
                 order by
                     v.name
             ";
             using (var connection = _connectionProvider.GetConnection())
             {
                 keywords = "%" + keywords + "%";
-                return connection.Query<SearchResult>(query, new { keywords }).ToList();
+                return connection.Query<SearchResult>(query, new { keywords, userId = User.CurrentUser.Id }).ToList();
             }
         }
     }
