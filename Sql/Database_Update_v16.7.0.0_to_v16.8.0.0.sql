7a67e3ea0995745378244014ab617565c381870b
Dmitry Teplov <dmitry.teplov@opencbs.com>
2016-08-10 13:44:51 +0600

    Add new column to update/create db script
    Also add 2 new type of event in dbo.EventTypes
    
    OP-149

--- Sql/Database_Update_v16.7.0.0_to_v16.8.0.0.sql
+++ Sql/Database_Update_v16.7.0.0_to_v16.8.0.0.sql
@@ -5,3 +5,9 @@ set
 where
     name = 'VERSION'
 GO
+
+IF NOT EXISTS ( SELECT * FROM sys.columns WHERE  object_id = OBJECT_ID(N'[dbo].[EventTypes]') AND name = 'parent_event_id' )
+BEGIN
+    ALTER TABLE dbo.EventTypes ADD [parent_event_id] int
+END
+GO
