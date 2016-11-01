b49af8b76e87ce5dbb87e792b3d14b0d37eb3e2b
Dmitry <dmitry.teplov@opencbs.com>
2016-11-01 13:55:01 +0600

    Rerurn previous round of OCurency type
    OP-222

--- Src/OpenCBS.Shared/OCurrency.cs
+++ Src/OpenCBS.Shared/OCurrency.cs
@@ -24,42 +24,42 @@ using System.Globalization;
 
 namespace OpenCBS.Shared
 {
-	[Serializable]
-	public struct OCurrency
-	{
+    [Serializable]
+    public struct OCurrency
+    {
         private decimal? _currency;
         public static bool UseCents = true;
 
-		public OCurrency(decimal a)
-		{
-			_currency = a;
-		}
-		public OCurrency(decimal? a)
-		{
-		    _currency = a.HasValue ? a : null;
-		}
+        public OCurrency(decimal a)
+        {
+            _currency = a;
+        }
+        public OCurrency(decimal? a)
+        {
+            _currency = a.HasValue ? a : null;
+        }
 
-	    public OCurrency(OCurrency obj)
-		{
-			_currency = obj._currency;
-		}
+        public OCurrency(OCurrency obj)
+        {
+            _currency = obj._currency;
+        }
 
-		public decimal Value
-		{
-			get { return _currency.Value; }
-		}
+        public decimal Value
+        {
+            get { return _currency.Value; }
+        }
 
-		public string RoundingValue
-		{
-			get 
+        public string RoundingValue
+        {
+            get
             {
                 var val = _currency ?? 0;
                 var roundPrecision = UseCents ? 2 : 0;
                 var fmt = UseCents ? "{0:N2}" : "{0:N0}";
                 val = Math.Round(val, roundPrecision, MidpointRounding.AwayFromZero);
                 return string.Format(fmt, val);
-			}
-		}
+            }
+        }
 
         public string GetFormatedValue(bool useCents)
         {
@@ -80,47 +80,47 @@ namespace OpenCBS.Shared
             return val.ToString("N", nfi);
         }
 
-		public bool HasValue
-		{
-			get { return _currency.HasValue; }
-		}
+        public bool HasValue
+        {
+            get { return _currency.HasValue; }
+        }
 
-		public static implicit operator OCurrency(decimal? source)
-		{
-		    return source.HasValue ? new OCurrency(Math.Round(source.Value, 2, MidpointRounding.AwayFromZero)) : new OCurrency(source);
-		}
+        public static implicit operator OCurrency(decimal? source)
+        {
+            return source.HasValue ? new OCurrency(Math.Round(source.Value, 4, MidpointRounding.AwayFromZero)) : new OCurrency(source);
+        }
 
-	    public static OCurrency operator +(OCurrency a, OCurrency b)
-	    {
-	        return a.HasValue && b.HasValue
-	                   ? new OCurrency(Math.Round((a._currency.Value + b._currency.Value), 2, MidpointRounding.AwayFromZero))
-	                   : null;
-	    }
+        public static OCurrency operator +(OCurrency a, OCurrency b)
+        {
+            return a.HasValue && b.HasValue
+                       ? new OCurrency(Math.Round((a._currency.Value + b._currency.Value), 4, MidpointRounding.AwayFromZero))
+                       : null;
+        }
 
-	    public static OCurrency operator -(OCurrency a, OCurrency b)
-	    {
-	        return a.HasValue && b.HasValue
-	                   ? new OCurrency(Math.Round((a._currency.Value - b._currency.Value), 2, MidpointRounding.AwayFromZero))
-	                   : null;
-	    }
+        public static OCurrency operator -(OCurrency a, OCurrency b)
+        {
+            return a.HasValue && b.HasValue
+                       ? new OCurrency(Math.Round((a._currency.Value - b._currency.Value), 4, MidpointRounding.AwayFromZero))
+                       : null;
+        }
 
-	    public static OCurrency operator *(OCurrency a, OCurrency b)
-	    {
-	        return a.HasValue && b.HasValue
-	                   ? new OCurrency(Math.Round((a._currency.Value*b._currency.Value), 2, MidpointRounding.AwayFromZero))
-	                   : ((a.HasValue && a.Value == 0) | (b.HasValue && b.Value == 0) 
-                                    ? (OCurrency) 0 
+        public static OCurrency operator *(OCurrency a, OCurrency b)
+        {
+            return a.HasValue && b.HasValue
+                       ? new OCurrency(Math.Round((a._currency.Value * b._currency.Value), 4, MidpointRounding.AwayFromZero))
+                       : ((a.HasValue && a.Value == 0) | (b.HasValue && b.Value == 0)
+                                    ? (OCurrency)0
                                     : null);
-	    }
+        }
 
-	    public static OCurrency operator *(OCurrency a, double b)
-	    {
-	        if (!a.HasValue)
-	            return null;
+        public static OCurrency operator *(OCurrency a, double b)
+        {
+            if (!a.HasValue)
+                return null;
 
-	        double resulDouble = Convert.ToDouble(a.Value)*b;
-	        return new OCurrency(Math.Round(Convert.ToDecimal(resulDouble), 2, MidpointRounding.AwayFromZero));
-	    }
+            double resulDouble = Convert.ToDouble(a.Value) * b;
+            return new OCurrency(Math.Round(Convert.ToDecimal(resulDouble), 4, MidpointRounding.AwayFromZero));
+        }
 
         public static OCurrency operator *(OCurrency a, decimal b)
         {
@@ -128,130 +128,130 @@ namespace OpenCBS.Shared
                 return null;
 
             decimal result = a.Value * b;
-            return new OCurrency(Math.Round(result, 2, MidpointRounding.AwayFromZero));
+            return new OCurrency(Math.Round(result, 4, MidpointRounding.AwayFromZero));
         }
 
-	    public static OCurrency operator *(OCurrency a, int b)
-	    {
-	        if (!a.HasValue)
-	            return null;
-
-	        decimal resulDecimal = Convert.ToDecimal(a.Value) * b;
-	        return new OCurrency(Math.Round(resulDecimal, 2, MidpointRounding.AwayFromZero));
-	    }
-
-	    public static OCurrency operator *(double a, OCurrency b)
-		{
-	        if (!b.HasValue)
-	            return null;
-	        
-            double resulDouble = Convert.ToDouble(b.Value) * a;
-	        return new OCurrency(Math.Round(Convert.ToDecimal(resulDouble), 2, MidpointRounding.AwayFromZero));
-		}
-
-	    public static OCurrency operator /(OCurrency a, OCurrency b)
-		{
-		    if (a.HasValue && b.HasValue)
-			{
-			    if (b.Value != 0)
-					return new OCurrency(Math.Round(a._currency.Value / b._currency.Value, 2, MidpointRounding.AwayFromZero));
-			    throw new OverflowException();
-			}
-		    return null;
-		}
-
-	    public static OCurrency operator /(OCurrency a, int b)
+        public static OCurrency operator *(OCurrency a, int b)
         {
-            if (a.HasValue && b != 0)
+            if (!a.HasValue)
+                return null;
+
+            decimal resulDecimal = Convert.ToDecimal(a.Value) * b;
+            return new OCurrency(Math.Round(resulDecimal, 4, MidpointRounding.AwayFromZero));
+        }
+
+        public static OCurrency operator *(double a, OCurrency b)
+        {
+            if (!b.HasValue)
+                return null;
+
+            double resulDouble = Convert.ToDouble(b.Value) * a;
+            return new OCurrency(Math.Round(Convert.ToDecimal(resulDouble), 4, MidpointRounding.AwayFromZero));
+        }
+
+        public static OCurrency operator /(OCurrency a, OCurrency b)
+        {
+            if (a.HasValue && b.HasValue)
             {
-                return new OCurrency(Math.Round(a._currency.Value / b, 2, MidpointRounding.AwayFromZero));
+                if (b.Value != 0)
+                    return new OCurrency(Math.Round(a._currency.Value / b._currency.Value, 4, MidpointRounding.AwayFromZero));
+                throw new OverflowException();
             }
             return null;
         }
 
-	    public static OCurrency operator /(OCurrency a, double b)
-		{
+        public static OCurrency operator /(OCurrency a, int b)
+        {
+            if (a.HasValue && b != 0)
+            {
+                return new OCurrency(Math.Round(a._currency.Value / b, 4, MidpointRounding.AwayFromZero));
+            }
+            return null;
+        }
+
+        public static OCurrency operator /(OCurrency a, double b)
+        {
             if (b == 0) throw new OverflowException();
-			return a.HasValue ? new OCurrency(Math.Round(a._currency.Value / (decimal)b, 2, MidpointRounding.AwayFromZero)) : null;
-		}
+            return a.HasValue ? new OCurrency(Math.Round(a._currency.Value / (decimal)b, 4, MidpointRounding.AwayFromZero)) : null;
+        }
 
         public static OCurrency operator /(OCurrency a, decimal b)
         {
             if (b == 0) throw new OverflowException();
-            return a.HasValue ? new OCurrency(Math.Round(a._currency.Value / b, 2, MidpointRounding.AwayFromZero)) : null;
+            return a.HasValue ? new OCurrency(Math.Round(a._currency.Value / b, 4, MidpointRounding.AwayFromZero)) : null;
         }
 
-		public static bool operator >(OCurrency a, OCurrency b)
-		{
-		    if (a.HasValue && b.HasValue)
-			{
-				return a._currency.Value > b._currency.Value;
-			}
-		    return a.HasValue && !b.HasValue;
-		}
+        public static bool operator >(OCurrency a, OCurrency b)
+        {
+            if (a.HasValue && b.HasValue)
+            {
+                return a._currency.Value > b._currency.Value;
+            }
+            return a.HasValue && !b.HasValue;
+        }
 
-	    public static bool operator <(OCurrency a, OCurrency b)
-	    {
-	        if (a.HasValue && b.HasValue)
-			{
-			    return a._currency.Value < b._currency.Value;
-			}
-	        return a.HasValue && !b.HasValue;
-	    }
+        public static bool operator <(OCurrency a, OCurrency b)
+        {
+            if (a.HasValue && b.HasValue)
+            {
+                return a._currency.Value < b._currency.Value;
+            }
+            return a.HasValue && !b.HasValue;
+        }
 
-	    public static bool operator >=(OCurrency a, OCurrency b)
-	    {
-	        if (a.HasValue && b.HasValue)
-			{
-			    return a._currency.Value > b._currency.Value | a._currency.Value == b._currency.Value;
-			}
-	        return a.HasValue && !b.HasValue;
-	    }
+        public static bool operator >=(OCurrency a, OCurrency b)
+        {
+            if (a.HasValue && b.HasValue)
+            {
+                return a._currency.Value > b._currency.Value | a._currency.Value == b._currency.Value;
+            }
+            return a.HasValue && !b.HasValue;
+        }
 
-	    public static bool operator <=(OCurrency a, OCurrency b)
-	    {
-	        if (a.HasValue && b.HasValue)
-	            return a._currency.Value < b._currency.Value | a._currency.Value == b._currency.Value;
+        public static bool operator <=(OCurrency a, OCurrency b)
+        {
+            if (a.HasValue && b.HasValue)
+                return a._currency.Value < b._currency.Value | a._currency.Value == b._currency.Value;
 
-	        return a.HasValue && !b.HasValue;
-	    }
+            return a.HasValue && !b.HasValue;
+        }
 
-	    public static bool operator ==(OCurrency a, OCurrency b)
-		{
-		    if (a.HasValue && b.HasValue)
-		        return a._currency.Value == b._currency.Value;
-		    return !a.HasValue && !b.HasValue;
-		}
+        public static bool operator ==(OCurrency a, OCurrency b)
+        {
+            if (a.HasValue && b.HasValue)
+                return a._currency.Value == b._currency.Value;
+            return !a.HasValue && !b.HasValue;
+        }
 
-	    public static bool operator !=(OCurrency a, OCurrency b)
-		{
-			if (a.HasValue && b.HasValue)
-			{
-			    return a._currency.Value > b._currency.Value | a._currency.Value < b._currency.Value;
-			}
-	        return a != null || b != null;
-		}
+        public static bool operator !=(OCurrency a, OCurrency b)
+        {
+            if (a.HasValue && b.HasValue)
+            {
+                return a._currency.Value > b._currency.Value | a._currency.Value < b._currency.Value;
+            }
+            return a != null || b != null;
+        }
 
-		public override int GetHashCode()
-		{
-			return Value.GetHashCode();
-		}
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
 
-		public override bool Equals(object b)
-		{
-			if (HasValue)
-			{
-			    if(b is OCurrency)
-    			    return Value.Equals(((OCurrency)b).Value);
-			    
+        public override bool Equals(object b)
+        {
+            if (HasValue)
+            {
+                if (b is OCurrency)
+                    return Value.Equals(((OCurrency)b).Value);
+
                 return Value.Equals(Convert.ToDecimal(b));
-			}
-		    return !HasValue && b == null;
-		}
+            }
+            return !HasValue && b == null;
+        }
 
-		public override string ToString()
-		{
-		    return _currency.ToString();
-		}
-	}
+        public override string ToString()
+        {
+            return _currency.ToString();
+        }
+    }
 }
