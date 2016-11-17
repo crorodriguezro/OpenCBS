8d90085fe6f4c1b811e26f91096ff6ed9991e980
Dmitry <dmitry.teplov@opencbs.com>
2016-11-17 14:20:32 +0600

    Add new parameter for refresh extention method
    Also execute this method for save loan approval action

--- Src/OpenCBS.Extensions/IClientFormInitializer.cs
+++ Src/OpenCBS.Extensions/IClientFormInitializer.cs
@@ -1,10 +1,11 @@
 ﻿using System.Windows.Forms;
+using OpenCBS.CoreDomain.Contracts.Loans;
 
 namespace OpenCBS.Extensions
 {
     public interface IClientFormInitializer
     {
         void Initialize(Form form);
-        void Refresh(Form form);
+        void Refresh(Form form, Loan loan = null);
     }
 }
