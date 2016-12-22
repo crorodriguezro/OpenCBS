079175eccbea510a4e1059a182d0a725c3503a9c
Askat <askat.asanali@opencbs.com>
2016-12-22 15:35:12 +0600

    Add installment number in dbo.LoanPenaltyAccrualEvents
    OP-265

--- Sql/Database_Update_v16.11.0.0_to_v16.12.0.0.sql
+++ Sql/Database_Update_v16.11.0.0_to_v16.12.0.0.sql
@@ -2,3 +2,9 @@ UPDATE  [TechnicalParameters]
 SET     [value] = 'v16.12.0.0'
 WHERE   [name] = 'VERSION'
 GO
+
+if col_length('dbo.LoanPenaltyAccrualEvents','installment_number') IS NULL
+    begin
+        alter table dbo.LoanPenaltyAccrualEvents add installment_number int not null
+    end
+GO
